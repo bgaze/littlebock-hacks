2d9d825176d41fb686d49f656a000aaad70281c0
bgaze <benjamin@bgaze.fr>
2023-06-04 21:51:43 +0400

    Recompute fermentables & hops based on a total value

--- assets/recipe.js
+++ assets/recipe.js
@@ -1,14 +1,28 @@
-function manageTotalAmount(container, items, unit) {
-    let $parent = $(container).parent();
+function makeTotalWidget(container, unit) {
+    let $parent = $(`#${container}`).parent();
 
-    if (!$('.total-amount', $parent).length) {
-        $('h4.header-title', $parent).append('<span class="total-amount d-inline-block ml-1">');
-    }
+    let $div = $('<div class="d-flex align-item-center justify-content-between mb-2" style="border-bottom: 1px solid #eceff1;">').prependTo($parent);
 
+    $('h4.header-title', $parent).addClass('m-0').appendTo($div).append(`<span id="${container}-total-amount" class="total-amount d-inline-block ml-1">`);
+
+    $div.append(`  <div id="${container}-total-widget" class="remove-wrapper d-flex align-item-center ml-3">
+                        <div class="form-group mb-0 mr-1">
+                            <div class="input-group">
+                                <input type="text" class="form-control" value="" style="width: 100px;" placeholder="Total">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">${unit}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="button" class="btn btn-sm btn-primary" style="height: 31px"><i class="mdi mdi-arrow-right-bold"></i></button>
+                    </div>`);
+}
+
+function manageTotalAmount(container, items, unit, format) {
     let sum = 0;
     let ok = true;
 
-    $(items).each(function () {
+    $(items, `#${container}`).each(function () {
         let val = parseFloat($(this).val().replace(',', '.'));
 
         if (!isNaN(val)) {
@@ -18,7 +32,33 @@ function manageTotalAmount(container, items, unit) {
         }
     });
 
-    $('.total-amount', $parent).toggleClass('text-danger', !ok).text(ok ? `(${sum} ${unit})` : '(erreur !)');
+    if (ok) {
+        sum = Math.round(sum * format) / format;
+    }
+
+    $(`#${container}-total-widget`).toggleClass('d-none', !(ok && sum > 0)).toggleClass('d-flex', ok && sum > 0);
+
+    $(`#${container}-total-amount`)
+        .toggleClass('text-danger', !ok)
+        .text(ok ? `(${sum} ${unit})` : '(erreur !)')
+        .data('total', ok ? sum : false);
+}
+
+function computeBasedOnTotal(container, items, currentTotal, format) {
+    let $input = $(`#${container}-total-widget input`).removeClass('is-invalid');
+
+    let newTotal = parseFloat($input.val().replace(',', '.'));
+    if (isNaN(newTotal) || newTotal <= 0) {
+        $input.addClass('is-invalid').focus();
+        return;
+    }
+
+    $(items).each(function () {
+        let value = parseFloat($(this).val().replace(',', '.'))
+        $(this).val(Math.round(value / currentTotal * newTotal * format) / format);
+    });
+
+    $input.val('');
 }
 
 $(document).ready(function () {
@@ -27,8 +67,27 @@ $(document).ready(function () {
     $('#yeasts').parents('#form_recipe > .row').insertAfter('#form_recipe > .row:first');
 
     // Manage fermentables & hops total
+    makeTotalWidget('fermentables', 'kg');
+    makeTotalWidget('hops', 'IBU');
     setInterval(function () {
-        manageTotalAmount('#fermentables', '.fermentable-amount', 'kg');
-        manageTotalAmount('#hops', '.hop-amount', 'g');
+        manageTotalAmount('fermentables', '.fermentable-amount', 'kg', 1000);
+        manageTotalAmount('hops', '.hop-amount', 'g', 10);
     }, 500);
+
+    // Set fermentables total amount
+    $('#fermentables-total-widget button').click(function () {
+        computeBasedOnTotal('fermentables', '.fermentable-amount', $(`#fermentables-total-amount`).data('total'), 1000);
+    });
+
+    // Set fermentables total amount
+    $('#hops-total-widget button').click(function () {
+        let ibu = 0;
+
+        $('#hops .info-data-hops-ibu').each(function () {
+            ibu += parseFloat($(this).text());
+        });
+
+        computeBasedOnTotal('hops', '.hop-amount', ibu, 1);
+    });
 });
+
